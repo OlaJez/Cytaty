c8654343091409b9bf54d09864b6a7946c3b9358
Ola Jeżewska <ola.jeewska@gmail.com>
2019-01-03 12:50:19 +0100

    Init

---
+++ Cytaty/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cytaty.MvcApplication" Language="C#" %>
---
+++ Cytaty/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cytaty.Startup))]
+namespace Cytaty
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
