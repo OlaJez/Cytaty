3c0f4e9ef6a976f660f5d55b50bcd2947b6c5b69
Ola Jeżewska <ola.jeewska@gmail.com>
2019-01-09 23:14:28 +0100

    dodanie skryptu JS na stronę główną. Losowanie cytatu z dostępnych.

---
+++ Cytaty/Scripts/LosujCytat.js
@@ -0,0 +1,15 @@
+﻿function losujCytat() {
+    //podloga - odcina/ random od 0....1/* ilosc cytatow
+    var wylosowany_cytat = Math.floor(Math.random() * $("div.opakowany-cytat").length);
+    //ktory wylosowany
+    console.log(wylosowany_cytat);
+    //$ - wywołanie jQuery
+    $("div.opakowany-cytat").hide();
+    //konwertuje obiekt html wylosowany cyt do obiektu jQuery
+    $($("div.opakowany-cytat")[wylosowany_cytat]).show();
+}
+
+losujCytat();
+
+//samo sie odswieza po 10 sek
+setInterval(losujCytat, 3000);
